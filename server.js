fcd5fcb7b2fb6a254cbdd26e4911bc7e6abfa20a
Scott Amaranto <amaranto.scott@gmail.com>
2021-11-28 16:00:31 -0500

    fixed databases bug

--- server.js
+++ server.js
@@ -2,7 +2,8 @@
 var express = require("express")
 var app = express()
 // Require database SCRIPT file
-var db = require("./database.js")
+var db1 = require("./userdatabase.js")
+var db2 = require("./transactiondatabase.js")
 // Require md5 MODULE
 var md5 = require("md5")
 // Require cors
@@ -24,6 +25,67 @@ app.get("/app/", (req, res, next) => {
     res.json({"message":"Your API works! (200)"});
 	res.status(200);
 });
+//CREATE a new user at endpoint /app/new/user
+app.post("/app/new/user", (req, res, next) => { //may be /app/new/user
+	console.log("user added to db1 with data: "+ req.body);
+	var data = {
+		user: req.body.user,
+		pass: req.body.pass ? md5(req.body.pass) : null,
+		email: req.body.email,
+		lastlogin: req.body.lastlogin
+	}
+	const stmt = db1.prepare("INSERT INTO userinfo (user, pass, email, lastlogin) VALUES (?, ?, ?, ?)");
+	const info = stmt.run(req.body.user, req.body.pass, req.body.email, req.body.lastlogin);
+	res.status(201).json({"message":stmt.changes + " record created: ID " + info.lastInsertRowid + " (201)"});
+});
+// DELETE a single user (HTTP method DELETE) at endpoint /app/delete/user/:id
+app.delete("/app/delete/user/:id", (req, res) => {
+	const stmt = db1.prepare("DELETE FROM userinfo WHERE id = ?");
+	const stmt2 =db2.prepare("DELETE * FROM transactions WHERE id = ?");
+
+	const info = stmt.run(req.params.id);
+
+	res.status(200).json({"message":info.changes + " record deleted: ID " + req.params.id + " (200)"});
+
+});
+// READ a single user (HTTP method GET) at endpoint /app/user/:id
+app.get("/app/user/:id", (req, res) => {	
+	const stmt = db1.prepare("SELECT * FROM userinfo WHERE id = ?").get(req.params.id);
+	res.json(stmt);
+    res.status(200);
+});
+
+// UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
+app.patch("/app/update/user/:id", (req, res) => {
+	var data = {
+        user: req.body.user,
+        pass: req.body.pass ? md5(req.body.pass) : null,
+        email: req.body.email,
+		lastlogin: req.body.lastlogin
+    }
+    
+    const stmt = db.prepare("UPDATE userinfo SET user = COALESCE(?, user), pass = COALESCE(?, pass), email = COALESCE(?, email), lastlogin = COALESCE(?, lastlogin) WHERE id = ?");
+	const info = stmt.run(data.user, md5(data.pass), data.email, data.lastlogin, req.params.id);
+
+    res.status(201).json({"message":info.changes + " record updated: ID " + req.params.id + " (201)"});
+	res.json(stmt);
+	
+});
+
+//CREATE a new transaction at endpoint /app/new/transaction
+app.post("/app/new/transaction", (req, res, next) => { //may be /app/new/user
+	console.log("transaction added to db2 with data: "+ req.body);
+	var data = {
+		id: req.body.id,
+		tine: req.body.time,
+		category: req.body.category,
+		amount: req.body.amount
+	}
+	const stmt = db2.prepare("INSERT INTO transactions (id, time, category, amount) VALUES (?, ?, ?, ?)");
+	const info = stmt.run(data.id, data.time, data.category, data.amount);
+	res.status(201).json({"message":stmt.changes + " record created: ID " + info.lastInsertRowid + " (201)"});
+});
+
 // Default response for any other request
 app.use(function(req, res){
 	res.json({"message":"Endpoint not found. (404)"});
